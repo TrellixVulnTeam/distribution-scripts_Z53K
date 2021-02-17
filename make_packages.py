6f46d50ec0a18025c2c501cd0d66dd38c519bc39
Sergio Siccha <sergio.siccha@posteo.net>
2021-02-17 13:01:36 +0100

    make_packages.py: remove shell=true from subprocess.run
    and add utils.verify_command_available("gh")

--- make_packages.py
+++ make_packages.py
@@ -5,12 +5,14 @@
 #
 # 3. Upload current packages as "packages for 4.11.3"
 
-import sys, urllib.request, subprocess
+import sys, subprocess
 import utils
 
 if len(sys.argv) != 2:
     raise Exception("usage : make_packages.py" \
                     + "<tag>")
+
+utils.verify_command_available("gh")
 
 TAG = sys.argv[1]
 # TAG_NUMBER is TAG without the leading "v"
@@ -20,11 +22,12 @@ IS_MAJOR_RELEASE = (TAG[-1] == "0")
 # TODO: this is a different step
 # TODO add notes. Also a CHANGES.md?
 # TODO: check that the tag TAG exists
-subprocess.run("gh release create " + TAG
-               + " --prerelease"
-               + " --title " + TAG
-               + " --notes \"test notes\"",
-               shell=True)
+# TODO: this throws an error if the release already exists
+subprocess.run(["gh", "release", "create", TAG,
+               "--prerelease",
+               "--title", TAG,
+               "--notes", "\"test notes\""],
+               check=True)
 
 URL_TO_PACKAGE_ARCHIVES = \
     "https://github.com/gap-system/gap-distribution/releases/download/package-archives/"
@@ -59,7 +62,8 @@ utils.download(URL_TO_PACKAGE_ARCHIVES + PACKAGES_REQUIRED_SRC,
 # TODO: what happens if this crashes during the upload?
 print("Uploading " + PACKAGES_DST + " and " + PACKAGES_REQUIRED_DST)
 # --clobber means overwrite files with the same name
-subprocess.run("gh release upload --clobber " + TAG
-               + " " + PACKAGES_DST
-               + " " + PACKAGES_REQUIRED_DST,
-               shell=True)
+subprocess.run("gh", "release", "upload", "--clobber",
+               TAG,
+               PACKAGES_DST,
+               PACKAGES_REQUIRED_DST,
+               check=True)
