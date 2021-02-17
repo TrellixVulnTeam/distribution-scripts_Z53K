7d81af4d9ca72e257a06bc6daf884ea7d9ad84b8
Max Horn <max@quendi.de>
2021-02-17 15:01:38 +0100

    progress on make_tarball

--- make_tarball.py
+++ make_tarball.py
@@ -4,7 +4,7 @@
 # <https://hackmd.io/AWds-AnZT72XXsbA0oVC6A>, i.e.:
 #
 # 4. Create archives: run a script which takes a git ref (a sha1, a tag
-# such  "v4.11.3") as argument. 
+# such  "v4.11.3") as argument.
 
 
 # TODO: maybe move this to the gap repository (say to `etc`), and let it
@@ -35,7 +35,7 @@
 #  - if version was given:
 #    - create annotated tag `v4.X.Y` via `git tag -m "Version 4.X.Y" v4.X.Y`
 #    - push tag to github
-#    - upload files to GitHub release: new tarballs and also package tarballs to 
+#    - upload files to GitHub release: new tarballs and also package tarballs to
 #      to https://github.com/gap-system/gap/releases/tag/v4.X.Y
 #      (see for example <https://github.com/gap-system/gap/releases/tag/v4.11.0>)
 #  - if used on GitHub actions, the resulting files could be turned into "artifacts"
@@ -50,41 +50,115 @@
 
 from utils import *
 
+import shutil
+import subprocess
+import sys
+import tarfile
+import tempfile
 
 # Insist on Python >= 3.6 for f-strings and other goodies
 if sys.version_info < (3,6):
     error("Python 3.6 or newer is required")
 
-if len(sys.argv) < 2 or len(sys.argv) > 3:
-    error('usage: ' + sys.argv[0] + ' <gitref> [packagetarball]')
+# if len(sys.argv) < 2 or len(sys.argv) > 3:
+#     error('usage: ' + sys.argv[0] + ' <gitref> [packagetarball]')
+#
+# # TODO: make the gitref optional? then use the current HEAD commit
+# # How do we figured out if it refers to a versioned release? well
+# # either check if there is a v4.X.Y tag pointing to HEAD, OR just say
+# # that this mode of operation is not supported?
+# # Another option is to have a flag to select between "nightly release"
+# # and "proper release"???
+# #
+# # Let's see, here are two hypothetical invocations and what could happen:
+# #
+# #  $ ./make_tarball.py
+# #  The version set in configure.ac is 4.X.Y; using existing tag v4.X.Y
+# #  ....
+# #
+# #  $ ./make_tarball.py
+# #  The version set in configure.ac is 4.X.Y; shall I create and use a tag v4.X.Y? (Y/n)
+# #  ....
+# #
+# #  $ ./make_tarball.py --nightly
+# #  The version set in configure.ac is 4.X.Y; continuing in nightly mode
+# #  ....
+# gitref = sys.argv[1]
+# if len(gitref) <= 2:
+#     error(f"gitref {gitref} is too short")
+#
+# # TODO: verify the
+#
+# if all(c in string.hexdigits for c in s) and len(s) <= 20:
+#     # look like a sha1ref
+#     is_version_ref = False
+# elif:
+#     pass
+# else:
+#     error("unsupported gitref")
+#
+# if len(sys.argv) >= 3:
+#     packagetarball = sys.argv[2]
+# else:
+#     error("omitting packagetarball name not yet supported")
+#     # TODO: we could do this: if the gitref is a tag of the format v4.X.Y,
+#     # use that to derive the name of package tarball(s)
+#     # otherwise, use `git branch --contains SHA1` to determine the relevant
+#     # branch (either the highest numbered stable-X.Y, else master/main)
+#
 
-gitref = sys.argv[1]
-if len(sys.argv) >= 3:
-    packagetarball = sys.argv[2]
-else:
-    error("omitting packagetarball name not yet supported")
-    # here is what we can do: if the gitref is a tag of the format v4.X.Y,
-    # use that to derive the name of package tarball(s)
-    # otherwise, use `git branch --contains SHA1` to determine the relevant
-    #  branch (either the highest numbered stable-X.Y, else master/main)
-
-
+notice("Checking prerequisites")
 verify_command_available("curl")
 verify_command_available("git")
-
+verify_command_available("autoconf")
+verify_command_available("make")
+verify_git_repo()
 verify_git_clean()
 
-sys.exit(0)
-
-print("")
+subprocess.run(["git", "fetch", "--tags"], check=True)
 
 
-# or maybe the package tarball should already be there, and its name specified as a second argument
+basename = "gap-4.X.Y" # TODO/FIXME insert proper name
+#with tempfile.TemporaryDirectory() as tmpdir:
+tmpdir = "/tmp/foobar"
+notice(f"tmpdir = {tmpdir}")
 
+notice("Exporting repository content via `git archive`")
+subprocess.run(["git", "archive",
+                f"--prefix={basename}/",
+                f"--output={tmpdir}/{basename}.tar",
+                "HEAD"], check=True)
 
-#     - fetches the code from the given git ref in the GAP repository
+notice("Extracting exported content")
 
-print("TODO: invoke `git archive`") # or perhaps `git clone`
+with tarfile.open(f"{tmpdir}/{basename}.tar") as tar:
+    tar.extractall(path=tmpdir)
+
+badfiles = [
+".appveyor.yml",
+".codecov.yml",
+".gitattributes",
+".gitignore",
+".mailmap",
+".travis.yml",
+]
+
+with working_directory(tmpdir + "/" + basename):
+    subprocess.run(["ls"], check=True)
+    subprocess.run(["./autogen.sh"], check=True)
+    subprocess.run(["./configure"], check=True)
+    subprocess.run(["make", "-j8"], check=True)
+    #subprocess.run(["make", "bootstrap-pkg-full"], check=True)
+    subprocess.run(["make", "doc"], check=True)
+    subprocess.run(["make", "distclean"], check=True)
+    shutil.rmtree("benchmark")
+    shutil.rmtree("dev")
+    shutil.rmtree(".github")
+    for f in badfiles:
+        try:
+            os.remove(f)
+        except:
+            pass
 
 #     - fetches the versioned packages tarball
 
@@ -98,60 +172,7 @@ print("TODO: download package distro tarballs")
 print("TODO: extract package archive, either via Python package or by invoking `tar`")
 
 pkgarchive = "https://github.com/gap-system/gap-distribution/releases/download/package-archives/packages-stable-4.11.tar.gz"
-# TODO: for now use the above, in the future 
-
-#     - run autogen.sh, configure
-
-print("TODO: run autogen.sh") # needed anyway
-
-
-print("TODO: run configure")  # needed for `make doc`
-#  but beware: we must then also clean some of the files this produces
-
-
-
-#     - build the GAP manual via `make doc`
-
-print("TODO: run `make doc`")
-
-
-
-#     - (possibly regenerates packages manuals as well???)
-#     - remove LaTeX build artifacts
-
-print("TODO: remove unwanted files")
-
-# see <https://github.com/gap-system/gap-distribution/blob/master/DistributionUpdate/patternscolor.txt>
-#
-#   # files and directories we never include
-#   -*.git/*
-#   -*.DS_Store
-#   # files we never include
-#   -*.o
-#   # excluded directories
-#   -benchmark/*
-#   -dev/*
-#   #
-#   # files and directories included in the main archive
-#   #
-#   +doc/ref/*
-#   +doc/tut/*
-#   +doc/hpc/*
-#   +doc/gapmacro.tex
-#   +doc/gapmacrodoc.tex
-#   +doc/manualbib.xml
-#   +doc/manualindex
-#   +doc/make_doc.in
-#   +doc/versiondata
-#   +doc/versiondata.in
-#   -doc/*
-#   +etc/emacs/*
-#   +etc/vim/*
-#   +etc/convert.pl
-#   -etc/*
-#   # everything else is included
-#   +*
-#   
+# TODO: for now use the above, in the future
 
 #     - run `tar`  to create `.tar.gz` (and perhaps also `.tar.xz`)
 
@@ -159,10 +180,13 @@ print("TODO: create tarballs from all")
 
 
 # TODO: how to name the tarballs?
-#  - gap-4.11.1.tar.gz  for branch v4.11.1 
+#  - gap-4.11.1.tar.gz  for branch v4.11.1
 #  - what do use for a random sha1? if we simply work in a git clone, we could
 #    use the output of `git --version` (resp. parse `cnf/GAP-VERSION-FILE`)
 
 # TODO: also create a gap-core-4.X.Y.tar.gz tarbll which omits the pkg dir
 #  (but unlike the current one, perhaps it perhaps should contain the manual)
 
+
+# The end
+notice("DONE")
