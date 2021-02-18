48ae3e66918080e801f7b6845c0ee054c9b25e38
Wilf Wilson <wilf@wilf-wilson.net>
2021-02-18 17:05:26 +0000

    Thursday night work on website script

--- update_website.py
+++ update_website.py
@@ -8,15 +8,25 @@
 # FIXME standardise on single or double quotes for strings
 
 #import subprocess
-#import argparse
+import argparse
 import sys
 import requests
 import datetime
+import dateutil.parser
 import tempfile
 import tarfile
 from utils import *
 from git import Repo
 
+
+################################################################################
+# Insist on Python >= 3.6
+if sys.version_info < (3,6):
+    error("Python 3.6 or newer is required")
+
+
+################################################################################
+# Utility functions
 def is_possible_gap4_release_tag(tag):
     tag = tag.split('.')
     return len(tag) == 3 and tag[0] == 'v4' and all(x.isdigit() for x in tag[1:])
@@ -24,107 +34,186 @@ def is_possible_gap4_release_tag(tag):
 def mb_bytes(nrbytes):
     return str(round(int(nrbytes) / (1024 * 1024)))
 
-# Verify the necessary pre-requisites
 
-# Insist on Python >= 3.6
-if sys.version_info < (3,6):
-    error("Python 3.6 or newer is required")
+################################################################################
+# Define the arguments
 
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+
+description=
+"""A script to update the GAP website according to a GAP release on GitHub.
+
+Run this from within a git clone of the GapWWW repository, checked out at the
+version you want to update from (most likely the master branch, up to date with
+the master branch of github.com/gap-system/GapWWW).
+
+This tool queries the GitHub releases API for the appropriate release, downloads
+a corresponding tarball, and extracts relevant data about the included packages
+from their PackageInfo.g files. The tools adds this information to website, and
+creates a pull request.""",
+
+epilog=
+"""Notes:
+* To learn how to create a GitHub access token, please consult
+  https://help.github.com/articles/creating-an-access-token-for-command-line-use/
+""")
+
+# TODO allow user to specify which branch to make the changes on?
+# TODO Let the user choose between pushing directly and making a PR?
+# TODO implement some or all of these
+#parser.add_argument('-p', '--push', action='store_true',
+#                    help='also peform the final push, completing the release')
+#parser.add_argument('-f', '--force', action='store_true',
+#                    help='if a release with the same name already exists: overwrite it')
+
+group = parser.add_argument_group('GAP release details')
+
+group.add_argument('-t', '--tag', type=str,
+                   help='git tag of the GAP release, e.g. v4.15.2 (default: latest release)')
+group.add_argument('-d', '--date', type=str,
+                   help='YYYY-MM-DD GAP release date (default: today\'s date)')
+group.add_argument('--use-github-date', action='store_true',
+                   help='use the GitHub release publish date as the GAP release date')
+
+group = parser.add_argument_group('Paths')
+
+group.add_argument('--tmpdir', type=str,
+                   help='path to the directory in which to download and extract the GAP release (default: a new temporary directory)')
+#group.add_argument('--gap', type=str,
+#                   help='path to a GAP executable (default: use the GAP release)')
+
+group = parser.add_argument_group('Repository details and access')
+
+group.add_argument('--token', type=str, # TODO implement
+                   help='GitHub access token')
+group.add_argument('--push-remote', type=str, default="origin",
+                   help='git remote to which the gap-4.X.Y branch is pushed (default: origin)')
+group.add_argument('--pr-remote', type=str, default="origin",
+                   help='git remote to which a PR is made (default: origin)')
+
+args = parser.parse_args()
+
+
+################################################################################
+# Validate the arguments
+
+if args.tag != None and not is_possible_gap4_release_tag(args.tag):
+    error('unrecognised GAP release tag: ' + args.tag)
+
+if args.use_github_date and args.date:
+    error('--use-github-date and --date/-d were both specified; use at most one')
+elif args.date:
+    try:
+        release_date = datetime.datetime.strptime(args.date, '%Y-%m-%d')
+    except ValueError:
+        error("--date YYYY-MM-DD")
+elif not args.use_github_date:
+    release_date = datetime.datetime.today()
+
+if (args.push_remote != None and len(args.push_remote) == 0) or (args.pr_remote != None and len(args.pr_remote) == 0):
+    error("--push-remote and --pr-remote must be nonempty")
+
+if args.tmpdir != None:
+    if not os.path.isdir(args.tmpdir):
+        # TODO check that it is writeable too
+        error("--tmpdir does not describe a directory")
+    tmpdir = args.tmpdir
+else:
+    tmpdir = tempfile.gettempdir()
+
+
+################################################################################
 # Verify that commands are available
 verify_command_available("gh")
 verify_command_available("git")
 verify_command_available("tar")
+# Verify that the pwd is a clean git repo
 verify_git_repo()
 verify_git_clean()
 
 
-# TODO Process arguments, including a -h, --help describing the script
-
-# TODO Add optional argument: name of remote to push to (default: origin)
-remote = 'origin'
-
-# TODO Add optional argument: git_tag of the release (default: '', to use the latest release)
-# TODO I am still implicitly assuming that the tag is
-release_name = ''
-assert(release_name == '' or is_possible_gap4_release_tag(release_name))
-
-# TODO Ensure we are in GapWWW repository
-# TODO Ensure we are on master, up to date with origin/master pointing at the right place
-# TODO git checkout -b NEW
-# TODO can this be different for push and pull?
-# TODO do this robustly!
-# TODO allow this to be git@github.com:gap-system/GapWWW.git too
-GapWWW_remotes = [ 'https://github.com/gap-system/GapWWW',\
-                   'git@github.com:gap-system/GapWWW.git' ]
-remote_url = subprocess.getoutput(["git config --get remote." + remote + ".url"])
-if remote_url[-1] == '/':
-    remote_url = remote_url[:-1]
-if not remote_url in GapWWW_remotes:
-    error("the remote '" + remote + "' must point to one of " + str(GapWWW_remotes) + \
-            " (not " + remote_url+ ")")
-#repo = Repo(os.getcwd())
-
-# Calculate release date
-today = datetime.datetime.today()
-year = today.strftime('%Y')
-month = today.strftime('%B')
-date = today.strftime('%d %B %Y') # FIXME is this really the format that we want?
-notice('release date will be ' + date)
-
-# Query GitHub API over https to find the most recent release on gap-system/gap
-# TODO Do this via PyGitHub?
+################################################################################
+# Use the GitHub API to find the release on github.com/gap-system/gap
 github_releases_url = 'https://api.github.com/repos/gap-system/gap/releases'
 request = requests.get(github_releases_url)
-request.raise_for_status() # TODO better error handling
-github_releases = request.json() # dictionary with latest GitHub release info
-if release_name == '':
-    release = github_releases[0]
+try:
+    request.raise_for_status()
+except:
+    error('failed to connect to GitHub API at ' + github_releases_url)
+
+# Select the appropriate release (as a dictionary)
+releases = request.json()
+releases = [ x for x in releases if is_possible_gap4_release_tag(x['tag_name']) ]
+if len(releases) == 0:
+    error("no GAP v4.X.Y releases found on GitHub!")
+if args.tag == None:
+    release = releases[0]
 else:
-    release = [ x for x in github_releases if x['tag_name'] == release_name ]
+    release = [ x for x in releases if x['tag_name'] == args.tag ]
     if len(release) != 1:
-        error("non-existent or amnbiguous release tag name " + release_nane)
+        error("non-existent or amnbiguous release tag name: " + args.tag)
     release = release[0]
 
-# Extract version number 4.X.Y and URL for gap-4.X.Y.tar.gz from the release dictionary
-# Assert that the tag has the form v4.\d+.\d+
-assert(is_possible_gap4_release_tag(release['tag_name']))
+# Release date
+if args.use_github_date:
+    release_date = dateutil.parser.isoparse(release['published_at'])
+year  = release_date.strftime('%Y')
+month = release_date.strftime('%B')
+date  = release_date.strftime('%d %B %Y')
+
+# Version
 gap_version = release['tag_name'][1:]
-tag = gap_version.split('.')
-gap_version_major = tag[1]
-gap_version_minor = tag[2]
-notice('Requested GAP release on github.com/gap-system/gap is ' + gap_version)
+gap_version_major, gap_version_minor = gap_version.split('.')[1:]
 
-# Download the unique release asset named 'gap-4.X.Y.tar.gz'
+notice("using git tag " + release['tag_name'])
+notice("using GAP release: " + gap_version)
+notice("using release date: " + date)
+notice("using temporary directory: " + tmpdir)
+
+
+################################################################################
+# Download and extrract the release asset named 'gap-4.X.Y.tar.gz'
 tarball = 'gap-' + gap_version + '.tar.gz'
-tarball_url = [ x['browser_download_url'] \
-                for x in release['assets'] if x['name'] == tarball ]
-assert(len(tarball_url) == 1)
-tarball_url = tarball_url[0]
-#tmpdir = tempfile.gettempdir()
-tmpdir = "/tmp/wilf_experiment/"
-notice("tmpdir = " + tmpdir)
-notice('downloading ' + tarball_url + ' to tmpdir')
-#download(tarball_url, tmpdir + tarball)
+tarball_url = [ x for x in release['assets'] if x['name'] == tarball ]
+try:
+    tarball_url = tarball_url[0]['browser_download_url']
+except:
+    error("cannot find " + tarball + " in release for at tag " + relrease['tag_name'])
 
+# TODO error handling
 with working_directory(tmpdir):
-    notice("extracting the GAP tarball to " + tmpdir)
-    #with tarfile.open(tarball) as tar:
-    #    tar.extractall()
-    #os.remove(tarball)
+    notice('downloading ' + tarball_url + ' to: ' + tmpdir)
+    download(tarball_url, tarball)
+    notice('extracting ' + tarball)
+    with tarfile.open(tarball) as tar:
+        tar.extractall()
+    notice('deleting ' + tarball)
+    os.remove(tarball)
 
 gaproot = tmpdir + 'gap-' + gap_version + '/'
-notice("Using GAP root " + gaproot)
+pkg_dir = gaproot + 'pkg/'
+gap_exe = gaproot + 'bin/gap.sh'
 
-# Compile GAP
+
+################################################################################
+# Compile newly-download GAP so that we can use its executable
+# TODO error handling
 with working_directory(gaproot):
+    notice("compiling downloaded GAP to use it for extracting package data")
     notice("running configure")
-    #with open("../configure.log", "w") as fp:
-    #    subprocess.run(["./configure"], check=True, stdout=fp)
-
+    with open("../configure.log", "w") as fp:
+        subprocess.run(["./configure"], check=True, stdout=fp)
     notice("building GAP")
-    #with open("../make.log", "w") as fp:
-    #    subprocess.run(["make"], check=True, stdout=fp)
+    with open("../make.log", "w") as fp:
+        subprocess.run(["make"], check=True, stdout=fp)
 
+notice("Using GAP root: " + gap_exe)
+notice("Using GAP executable: " + gap_exe)
+notice("Using GAP package director: " + pkg_dir)
+
+
+################################################################################
+# Rewrite _data/release.yml
 notice("writing new _data/release.yml file")
 with open('_data/release.yml', 'w') as release_yml:
     release_yml.write("# Release details concerning the most recently released GAP version\n")
@@ -134,32 +223,38 @@ with open('_data/release.yml', 'w') as release_yml:
     release_yml.write("year: '" + year + "'\n")
 subprocess.run(["git", "add", "_data/release.yml"], check=True)
 
-# FIXME Can we re-organise the website so that this is not necessary?
-notice("appending \"gap4" + gap_version_major + \
-       "dist: 'https://files.gap-system.org/gap-4." + gap_version_major + \
-       "/'\" to _data/gap.yml, if necessary")
-with open('_data/gap.yml', 'r+') as gap_yml:
-    for line in gap_yml:
-        if line.startswith("gap4" + gap_version_major + "dist:"):
-            break
-    else:
-        gap_yml.write("gap4" + gap_version_major + \
-                      "dist: 'https://files.gap-system.org/gap-4." + gap_version_major + "/'\n")
-        subprocess.run(["git", "add", "_data/data.yml"], check=True)
 
-# i.e. run script which iterates over all packages and generates YAML files for each
+################################################################################
+# Append to _data/gap.yml if necessary
+# TODO This is currently unnecessary for new releases since we're only linking
+# to downloads on GitHub; if this changes, this code should maybe be reinstated
+#
+#with open('_data/gap.yml', 'r+') as gap_yml:
+#    for line in gap_yml:
+#        if line.startswith("gap4" + gap_version_major + "dist:"):
+#            break
+#    else:
+#        notice("appending \"gap4" + gap_version_major + "dist: 'https://files.gap-system.org/gap-4." + gap_version_major + "/'\" to _data/gap.yml")
+#        gap_yml.write("gap4" + gap_version_major + "dist: 'https://files.gap-system.org/gap-4." + gap_version_major + "/'\n")
+#        subprocess.run(["git", "add", "_data/data.yml"], check=True)
+
+
+################################################################################
+# Update/rewrite the YAML data files for each package in pkg_dir
 notice("running etc/generate_package_yml_files_from_PackageInfo_files.sh")
-subprocess.run(["etc/generate_package_yml_files_from_packageinfo_files.sh", gaproot], check=True)
+subprocess.run(["etc/generate_package_yml_files_from_packageinfo_files.sh", pkg_dir, gap_exe], check=True)
 subprocess.run(["git", "add", "_Packages/*.html"], check=True)
 
+
+################################################################################
+# Rewrite _data/help.yml
 notice("running GAP on etc/LinksOfAllHelpSections.g")
-subprocess.run([gaproot + "bin/gap.sh", "-A", "-r", "-q", "etc/LinksOfAllHelpSections.g"], check=True)
+subprocess.run([gap_exe, "-A", "-r", "-q", "etc/LinksOfAllHelpSections.g"], check=True)
 subprocess.run(["git", "add", "_data/help.yml"], check=True)
 
 
 ################################################################################
-# Build the new file _Releases/4.X.Y.html required by the Jekyll based website
-# i.e. generate a YAML file with all required info about the release suitable for the NEW Jekyll based website
+# Write _Releases/4.X.Y.html
 release_file = "_Releases/" + gap_version + ".html"
 notice("writing a new release file: " + release_file)
 
@@ -207,6 +302,7 @@ Expert users can find the description of all installation options in the
     new_file.write("""
 </table>
 <p>
+
 You may also consider one of the
 <a href="../Download/alternatives.html">alternative distributions</a>.
 Note, however, that these are updated independently and may not yet
@@ -231,12 +327,14 @@ subprocess.run(["git", "add", release_file], check=True)
 # TODO Add option --force
 subprocess.run(["git", "checkout", "-b", "gap-" + gap_version], check=True)
 subprocess.run(["git", "commit", "-m", "'Update website for GAP " + gap_version + " release'"], check=True)
+verify_git_clean() # Make sure we didn't miss anything
+
 print("TODO: git push origin gap-" + gap_version )
 print("TODO: create pull request")
 
 sys.exit(0)
 
-# TODO Delete the temporary gap directory, when we are finished with it
+# TODO Delete the temporary gap directory and log files, when we are finished with it
 # TODO Download all package tarballs, and compute their sizes and sha256 checksums
 # TODO stfp tarballs from GitHub release system to gap-system.org
 # sftp gap-web@gap-web.host.cs.st-andrews.ac.uk
