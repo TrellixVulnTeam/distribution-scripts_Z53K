eb3ef764697b926a151ce2c2ad540d75ff07e990
Sergio Siccha <sergio.siccha@posteo.net>
2021-02-19 12:51:28 +0100

    WIP make_github_release

--- make_github_release.py
+++ make_github_release.py
@@ -7,49 +7,38 @@
 # The version of the gap release to be created can be provided as argument to
 # this script and is handled as in make_tarball.py.
 
-import sys, subprocess
 # If we do import * from utils, then initialize_github can't overwrite the
 # global GITHUB_INSTANCE and CURRENT_REPO variables.
 import utils
 import github
 
-if len(sys.argv) != 2:
-    raise Exception("usage : make_packages.py" \
-                    + "<tag>")
+try:
+    GAPVERSION = get_makefile_var("GAP_BUILD_VERSION")
+except:
+    error("Could not get GAP version")
+notice(f"Detected GAP version {GAPVERSION}")
 
-TAG = sys.argv[1]
-# TAG_NUMBER is TAG without the leading "v"
-TAG_NUMBER = TAG[1:len(TAG)]
-IS_MAJOR_RELEASE = (TAG[-1] == "0")
-
-# TODO: where should we put this: initialize_github and create release on
-# github
 utils.initialize_github()
 
-if utils.check_whether_github_release_exists(TAG):
-    utils.error("Release " + TAG + " already exists!")
-# TODO add notes. Also a CHANGES.md?
+if utils.check_whether_github_release_exists(GAPVERSION):
+    utils.error(f"Release {GAPVERSION} already exists!")
+
 CURRENT_BRANCH = get_makefile_var("PKG_BRANCH")
-RELEASE = utils.CURRENT_REPO.create_git_release(TAG, TAG, "test message",
+RELEASE_NOTE = f"For an overview of changes in GAP {GAPVERSION} see CHANGES.md file."
+RELEASE = utils.CURRENT_REPO.create_git_release(TAG, TAG, RELEASE_NOTE,
                                       target_commitish=CURRENT_BRANCH,
                                       prerelease=True)
 
-if IS_MAJOR_RELEASE:
-    BRANCH = "master"
-else:
-    MAJOR_VERSION = TAG.split(".")[1]
-    BRANCH = "stable-4." + MAJOR_VERSION
-
-PACKAGES_DST = "packages-" + TAG_NUMBER + ".tar.gz"
-# packages-stable-4.11.tar.gz -> packages-v4.11.1.tar.gz
-# packages-required-stable-4.11.tar.gz -> packages-required-v4.11.1.tar.gz
-PACKAGES_REQUIRED_DST = "packages-required-" + TAG_NUMBER + ".tar.gz"
+tmpdir = os.getcwd() + "/tmp"
+with working_directory(tmpdir):
+    manifest_filename = "__manifest_make_tarball"
+    with open(manifest_filename, 'r') as manifest_file:
+        manifest = manifest_file.read().splitlines()
 
 # Upload all assets to release
 try:
-    utils.notice("Uploading " + PACKAGES_DST)
-    RELEASE.upload_asset(PACKAGES_DST)
+    for filename in manifest:
+        utils.notice("Uploading " + filename)
+        RELEASE.upload_asset(filename)
 except github.GithubException:
     error("Error: The upload failed")
-
-# TODO: remove tmpdir that was created in make_tarball.py
