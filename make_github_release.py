549b8c7d3e4fff5f083800486ea9286e87ef8b02
Sergio Siccha <sergio.siccha@posteo.net>
2021-02-19 10:37:10 +0100

    WIP clean up make_github_release

--- make_github_release.py
+++ make_github_release.py
@@ -8,6 +8,7 @@ import sys, subprocess
 # If we do import * from utils, then initialize_github can't overwrite the
 # global GITHUB_INSTANCE and CURRENT_REPO variables.
 import utils
+import github
 
 if len(sys.argv) != 2:
     raise Exception("usage : make_packages.py" \
@@ -25,17 +26,10 @@ utils.initialize_github()
 if utils.check_whether_github_release_exists(TAG):
     utils.error("Release " + TAG + " already exists!")
 # TODO add notes. Also a CHANGES.md?
-# From https://stackoverflow.com/questions/6245570/how-to-get-the-current-branch-name-in-git#12142066
-CURRENT_BRANCH = subprocess.run(["git", "rev-parse", "--abbrev-ref", "HEAD"],
-                                text=True,
-                                capture_output=True)
-CURRENT_BRANCH = CURRENT_BRANCH.stdout.strip()
+CURRENT_BRANCH = get_makefile_var("PKG_BRANCH")
 RELEASE = utils.CURRENT_REPO.create_git_release(TAG, TAG, "test message",
                                       target_commitish=CURRENT_BRANCH,
                                       prerelease=True)
-
-URL_TO_PACKAGE_ARCHIVES = \
-    "https://github.com/gap-system/gap-distribution/releases/download/package-archives/"
 
 if IS_MAJOR_RELEASE:
     BRANCH = "master"
@@ -43,35 +37,16 @@ else:
     MAJOR_VERSION = TAG.split(".")[1]
     BRANCH = "stable-4." + MAJOR_VERSION
 
-# TODO: do not download this again, if the tar ball already exists, but
-# double-check that we're not using the wrong tar ball by accident.
-# Download and rename the package tar balls:
-# packages-master.tar.gz -> packages-4.11.0.tar.gz
-# packages-required-master.tar.gz -> packages-required-4.11.0.tar.gz
-PACKAGES_SRC = "packages-" + BRANCH + ".tar.gz"
 PACKAGES_DST = "packages-" + TAG_NUMBER + ".tar.gz"
-print("Downloading " + PACKAGES_SRC + " and renaming it to "
-      + PACKAGES_DST)
 # packages-stable-4.11.tar.gz -> packages-v4.11.1.tar.gz
 # packages-required-stable-4.11.tar.gz -> packages-required-v4.11.1.tar.gz
-utils.download(URL_TO_PACKAGE_ARCHIVES + PACKAGES_SRC,
-               PACKAGES_DST)
-
-PACKAGES_REQUIRED_SRC = "packages-required-" + BRANCH + ".tar.gz"
 PACKAGES_REQUIRED_DST = "packages-required-" + TAG_NUMBER + ".tar.gz"
-print("Downloading " + PACKAGES_REQUIRED_SRC + " and renaming it to "
-      + PACKAGES_REQUIRED_DST)
-utils.download(URL_TO_PACKAGE_ARCHIVES + PACKAGES_REQUIRED_SRC,
-               PACKAGES_REQUIRED_DST)
-
-# TODO: what happens if this crashes during the upload?
-utils.notice("Uploading " + PACKAGES_DST + " and " + PACKAGES_REQUIRED_DST)
 
 # Upload all assets to release
 try:
+    utils.notice("Uploading " + PACKAGES_DST)
     RELEASE.upload_asset(PACKAGES_DST)
-    RELEASE.upload_asset(PACKAGES_REQUIRED_DST)
-except:
+except github.GithubException:
     error("Error: The upload failed")
 
 # TODO: remove tmpdir that was created in make_tarball.py
